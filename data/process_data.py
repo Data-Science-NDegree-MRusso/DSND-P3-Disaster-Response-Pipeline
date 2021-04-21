2d6797a7473e9f51cc99f27b27ff9afd65fecdcc
Michelangelo Russo <michelangelo.russo@hotmail.com>
2021-04-21 15:38:31 +0200

    test commit

--- data/process_data.py
+++ data/process_data.py
@@ -6,11 +6,13 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
+    #clean data
     pass
 
 
 def save_data(df, database_filename):
-    pass  
+    # save data
+    pass
 
 
 def main():
@@ -24,12 +26,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
